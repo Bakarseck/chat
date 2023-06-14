7bc6706d14a79d85cb55f7a492a201ac74d3d6cd
bakseck <seck.bakar@ugb.edu.sn>
2023-06-14 12:45:55 +0000

    sever

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"bufio"
 )
 
 func checkError(err error) {
@@ -26,30 +27,33 @@ func main() {
 	ln, err := net.Listen("tcp", fmt.Sprintf("%s:%s", IP, PORT))
 	checkError(err)
 
-	//On accepte les connexions entrantes sur le port 3569
-	conn, err := ln.Accept()
-	checkError(err)
-
-	f("Un client est connecté depuis", conn.RemoteAddr())
+	var clients []net.Conn // tableau de clients
 	
 	for {
 
-		// On ecoute les messages émis par les connexions entrantes (ctrl-c pour quitter)
-
-		buffer := make([]byte, 4096) //Taille maximum du message qui sera envoyé par le client
-
-		length, err := conn.Read(buffer) // lire le message envoyé par le client
-
-		message := string(buffer[:length]) // supprimer les bits qui servent à rien et convertir les bytes en sring
-
-		if err != nil {
-			f("Le client s'est connecté")
+		conn, err := ln.Accept()
+		if err == nil {
+			clients = append(clients, conn)
 		}
+		checkError(err)
+		fmt.Println("Un client est connecté", conn.RemoteAddr())
+		
+		go func() {
+			buf := bufio.NewReader(conn)
 
-		// On affiche le message du client en le convertissant de byte à string
-		fmt.Print("Client:", message)
+			for {
+				name, err := buf.ReadString('\n')
 
-		// On envoie le message au client pour qu'il l'affiche
-		conn.Write([]byte(message + "\n"))
+				if err != nil {
+					fmt.Printf("Client disconnected.\n")
+					break
+				}
+
+				for _, c := range clients {
+					c.Write([]byte(name))
+				}
+			}
+
+		}()
 	}
 }
